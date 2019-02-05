4e8ee2e7dc782096fa7f1db4003b5781d5fa8c10
Gabor Horvath <xazax.hun@gmail.com>
2019-02-05 17:05:15 +0100

    Add some tests to track the progress with the warnings.

--- test/Sema/warn-lifetime-analysis-nocfg.cpp
+++ test/Sema/warn-lifetime-analysis-nocfg.cpp
@@ -1,6 +1,11 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -Wdangling-lifetime-pointer %s
+// RUN: %clang_cc1 -fsyntax-only -verify -Wdangling-lifetime-pointer -Waddress-of-temporary %s
+struct [[gsl::Owner]] MyOwner {
+  int &operator*();
+};
+
 struct [[gsl::Pointer]] MyPointer {
   MyPointer(int *p = 0);
+  MyPointer(const MyOwner &);
   int &operator*();
 };
 
@@ -10,11 +15,41 @@ struct T {
 
 void f() {
   new MyPointer(T{}); // expected-warning {{object backing the pointer will be destroyed at the end of the full-expression}}
+  new MyPointer(MyOwner{}); // expected-warning {{object backing the pointer will be destroyed at the end of the full-expression}}
 }
 
 void g() {
   int i;
-  MyPointer p{&i};
-  new MyPointer(MyPointer{p});
+  MyPointer p{&i}; // no-warning
+  new MyPointer(MyPointer{p}); // no-warning
 }
 
+struct Y {
+  int a[4];
+};
+
+void h() {
+  MyPointer p = Y{}.a; // expected-warning{{pointer is initialized by a temporary array}}
+  (void)p;
+}
+
+struct S {
+  MyPointer p; // expected-note {{pointer member declared here}}
+  S(int i)
+    : p(&i) {} // expected-warning {{initializing pointer member 'p' with the stack address of parameter 'i'}}
+  S() : p(T{}) {}
+  S(double) : p(MyOwner{}) {} // TODO
+};
+
+MyPointer i() {
+  int j;
+  return &j; // TODO
+}
+
+MyPointer global;
+
+void j() {
+  MyPointer p = MyOwner{}; // TODO
+  p = MyOwner{}; // TODO ?
+  global = MyOwner{}; // TODO ?
+}
